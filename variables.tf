191c0f65c992616cb20b0aeeec4b12e3bcc99e23
dsusmit63 <dsusmit63@gmail.com>
2026-05-12 03:05:37 +0530

    removed default values

--- variables.tf
+++ variables.tf
@@ -1,30 +1,32 @@
+variable "bucket_name" {
+  type = string
+  description = "Name of s3 bucket"
+}
+variable "bucket_tag" {
+  type = string
+  description = "Tag of s3 bucket"
+}
 variable "ec2_instance_count" {
   type = number
-  default = 1
   description = "Number of ec2 instance"
 }
 variable "ec2_instance_ami" {
   type = string
-  default = "ami-091138d0f0d41ff90"
   description = "AMI of ec2 instance"
 }
 variable "ec2_instance_type" {
   type = string
-  default = "t2.micro"
   description = "Type of ec2 instance"
 }
 variable "ec2_instance_root_volume_size" {
   type = number
-  default = 8
   description = "Volume size of ec2 instnace"
 }
 variable "ec2_instance_root_volume_type" {
   type = string
-  default = "gp3"
   description = "Volume type of ec2 instance"
 }
 variable "ec2_instance_name" {
   type = string
-  default = "My EC2 Instance"
   description = "Name of ec2 instance"
 }
