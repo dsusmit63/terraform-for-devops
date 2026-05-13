f056036b94296442d59163b55dc8252dc7f48227
dsusmit63 <dsusmit63@gmail.com>
2026-05-13 14:31:21 +0530

    modified outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
+output "local_file_name" {
+  value = local_file.myfile.filename
+}
+output "local_file_content" {
+  value = local_file.myfile.content
+}
+output "local_file_permission" {
+  value = local_file.myfile.file_permission
+}
 output "ec2_public_ip" {
   value = aws_instance.myec2instance[*].public_ip
 }
